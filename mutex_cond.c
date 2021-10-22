5a7dddfc185b5c0de2072e6186738001d85bcc7b
Valentina SERRA <vserra@e3r9p6.42.fr>
2021-10-22 19:23:26 +0200

    je suis une debutante 2

--- mutex_cond.c
+++ mutex_cond.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mutex.c                                            :+:      :+:    :+:   */
+/*   mutex_cond.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 18:01:09 by vserra            #+#    #+#             */
-/*   Updated: 2021/10/22 19:22:33 by vserra           ###   ########.fr       */
+/*   Updated: 2021/10/22 19:22:22 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,13 @@
 
 // variable globale pour l'exemple (de pref a mettredans une struct)
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 void	*hello1(void *data)
 {
-	// int *value = &((mutex*)data)->val;
 	char str[] = "hello world!";
-
 	int i = 0;
-	pthread_mutex_lock(&mutex); //bloquer le mutex pendant que tue fais les actions ci dessous
+	pthread_cond_wait(&cond, &mutex);
 	while (i <12)
 	{
 		fprintf(stderr, "%c", str[i]);
@@ -34,17 +33,13 @@ void	*hello1(void *data)
 	fprintf(stderr, "\n");
 
 	fprintf(stderr, "end1\n");
-	pthread_mutex_unlock(&mutex); // c bon tu peux debloquer
 	pthread_exit(NULL);
 }
 
 void	*hello2(void *data)
 {
-	// int *value = &((mutex*)data)->val;
 	char str[] = "Bye bye";
-
 	int i = 0;
-	pthread_mutex_lock(&mutex);
 	while (i < 7)
 	{
 		fprintf(stderr, "%c", str[i]);
@@ -54,7 +49,7 @@ void	*hello2(void *data)
 	fprintf(stderr, "\n");
 
 	fprintf(stderr, "end2\n");
-	pthread_mutex_unlock(&mutex);
+	pthread_cond_signal(&cond);
 	pthread_exit(NULL);
 }
 
@@ -65,7 +60,7 @@ int		main(void)
 
 	pthread_create(&t1, NULL, hello1, "Hello !");
 	pthread_create(&t2, NULL, hello2, "Bye, bye !");
-	pthread_mutex_destroy(&mutex);
+
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 
